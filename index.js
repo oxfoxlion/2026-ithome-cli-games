fa230f8f94000c6f33b3302bd4cc6298d2ee903a
Shao <shao@chengchitech.com>
2026-09-04 14:41:11 +0800

    day2 字串陣列地圖

--- index.js
+++ index.js
@@ -1,38 +1,88 @@
-let count = 3;
-let step = 0; // 紀錄走到了第幾步
+//地圖初始化
+const map = [
+    "########",
+    "#      #",
+    "#  .   #",
+    "#  $   #",
+    "#  @   #",
+    "#      #",
+    "########",
+];
 
-// 我們先往後推 7 行空地，確保把舊的「node index.js」指令推到上面去
-process.stdout.write("\n".repeat(7));
+let player = null; // 用來儲存玩家位置
+let box = null; // 用來儲存箱子位置
+let goal = null; // 用來儲存目標點位置
 
-// 空間推出來之後，立即回到左上，就不會被發現
-process.stdout.write("\x1b[7A");
+//用於計算三個物件的位置
+for (let y = 0; y < map.length; y++) {
+
+    for (let x = 0; x < map[y].length; x++) {
+
+        if (map[y][x] === '@') {
+            player = { x, y }
+        }
+        if (map[y][x] === '$') {
+            box = { x, y }
+        }
+        if (map[y][x] === '.') {
+            goal = { x, y }
+        }
+    }
+
+}
+
+//渲染畫面
+function render() {
+
+    //儲存整個畫面的字串
+    let frame = ''
+
+    for (let y = 0; y < map.length; y++) {
+        let row = ''
+
+        for (let x = 0; x < map[y].length; x++) {
+        
+        // 小優化，把常用的寫法儲存成變數
+        const cell = map[y][x]
+            
+            // 下面這邊把 map[y][x] 都替換成 cell
+            if(player.x === x && player.y === y){
+                row += '@'
+            }else if(box.x === x && box.y === y){
+                row += '$'
+            }else if(goal.x === x && goal.y === y){
+                row += '.'
+            }else if (cell === '@' || cell ==='$' || cell ==='.') {
+                row += ' '
+            } else {
+                row += cell
+            }
+
+        }
+
+        //把這行儲存進去 frame，並記得結尾換行
+        frame += row + '\n'
+    }
+
+    // 一樣先印出來看看結果
+    process.stdout.write("\x1b[7A");
+    process.stdout.write(frame)
+
+}
+
+//初次渲染
+process.stdout.write("\n".repeat(map.length));
+render()
+
+let count = 2;
 
 const timer = setInterval(() => {
-
-  // 為了避免重複的回到左上，第一次渲染就不用執行這一行
-  if (step > 0) {
-    process.stdout.write("\x1b[7A");
-  }
-
-  process.stdout.write('########\n');
-  process.stdout.write('#      #\n');
-  process.stdout.write('#  $.  #\n');
   
-  /// 第一行是 # 代表牆壁，起始時空兩格，第三格是 @ ，接著都是空格，最後是牆壁
-  /// 接下來每一秒渲染時都動態往後推一格
-  process.stdout.write('#' + " ".repeat(2 + step) + '@' + " ".repeat(3 - step) + '#\n');
-  
-  process.stdout.write('#      #\n');
-  process.stdout.write('########\n');
-  
-  // 印一條分隔線，方便看清楚每一秒的變化
-  process.stdout.write(`--- 第 ${step + 1} 秒的地圖狀態 --- \n`);
+  player.x += 1;
+  render()
+  count --;
 
-  count--;
-  step++; // 步數加 1，下一秒的 @ 就會往右挪一格
-
-  if (count < 0) {
+  if (count < 1) {
     clearInterval(timer);
   }
-
 }, 1000);
