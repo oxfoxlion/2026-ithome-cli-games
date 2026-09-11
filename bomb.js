6dc3762eeb9ae3e078f3f70e8f49c7b166b3aa32
Shao <oxfoxlion@gmail.com>
2026-09-11 12:11:54 +0800

    day10 印出10個箱子

--- bomb.js
+++ bomb.js
@@ -1,6 +1,6 @@
 //地圖初始化工具
 function createMap(width, height) {
-    
+
     const map = [];
 
     for (let y = 0; y < height; y++) {
@@ -10,7 +10,8 @@ function createMap(width, height) {
         for (let x = 0; x < width; x++) {
 
             const isBoder = y === 0 || y === height - 1 || x === 0 || x === width - 1;
-            if (isBoder) {
+            const isWall = y % 2 === 0 && x % 2 === 0;
+            if (isBoder || isWall) {
                 row.push('#')
             } else {
                 row.push(' ')
@@ -27,6 +28,64 @@ function createMap(width, height) {
 // 執行地圖初始化並計算出 map 
 const map = createMap(15, 9);
 
+// occupied 預設是一個空陣列，這樣也可以避免萬一沒傳入變數的情況，不會報錯
+function pickRandomEmptyCell(map, occupied = []) {
+    // 用來儲存空白格
+    const emptyCell = [];
+
+    for (let y = 0; y < map.length; y++) {
+
+        for (let x = 0; x < map[y].length; x++) {
+            // 判斷是不是已被占用
+            const isOccupied = occupied.some(cell => cell.x === x && cell.y === y);
+            // 如果該位置是空的而且尚未被占用才將這個數值儲存到 emptyCell
+            if (map[y][x] === " " && !isOccupied) {
+                emptyCell.push({ x, y })
+            }
+        }
+    }
+
+    const randomIndex = Math.floor(Math.random() * emptyCell.length);
+    return emptyCell[randomIndex]
+}
+
+function createBoxes(map,boxesNum, occupied = []) {
+
+    // 用來儲存箱子們的座標
+    let boxes = [];
+
+    // 找到所有的空格
+    const emptyCell = [];
+
+    for (let y = 0; y < map.length; y++) {
+
+        for (let x = 0; x < map[y].length; x++) {
+            // 判斷是不是已被占用
+            const isOccupied = occupied.some(cell => cell.x === x && cell.y === y);
+            // 如果該位置是空的而且尚未被占用才將這個數值儲存到 emptyCell
+            if (map[y][x] === " " && !isOccupied) {
+                emptyCell.push({ x, y })
+            }
+        }
+    }
+
+    // 隨機選 10 個位置
+    for (let i = emptyCell.length -1; i > 0; i --) { // 這裡是由大到小遞減的迴圈
+        const j = Math.floor(Math.random() * (i+1)); // 隨機在小於等於 i 的範圍內選一個數字
+        [emptyCell[i],emptyCell[j]] = [emptyCell[j],emptyCell[i]]; // 兩者交換
+    }
+
+    // 存進 boxes，這裡要記得帶入 boxesNum
+    boxes = emptyCell.slice(0,boxesNum);
+
+    return boxes;
+}
+
+let player = pickRandomEmptyCell(map);
+let enemy1 = pickRandomEmptyCell(map,[player]);
+let enemy2 = pickRandomEmptyCell(map,[player,enemy1]);
+let enemy3 = pickRandomEmptyCell(map,[player,enemy1,enemy2]);
+let boxes = createBoxes(map,10,[player,enemy1,enemy2,enemy3]);
 
 //渲染畫面
 function render() {
@@ -39,8 +98,20 @@ function render() {
         for (let x = 0; x < map[y].length; x++) {
 
             const cell = map[y][x]
-            
-            row += `\x1b[32m${cell}\x1b[0m`
+
+            if (player.x === x && player.y === y) {
+                row += `\x1b[33m@\x1b[0m`
+            }else if (enemy1.x === x && enemy1.y === y) {
+                row += `\x1b[31m&\x1b[0m`
+            }else if (enemy2.x === x && enemy2.y === y) {
+                row += `\x1b[31m&\x1b[0m`
+            }else if (enemy3.x === x && enemy3.y === y) {
+                row += `\x1b[31m&\x1b[0m`
+            }else if (boxes.some(box => box.x === x && box.y === y)) {
+                row += `\x1b[34mX\x1b[0m`  // 用藍色和 X 來呈現箱子
+            } else {
+                row += `\x1b[32m${cell}\x1b[0m`
+            }
 
         }
 
