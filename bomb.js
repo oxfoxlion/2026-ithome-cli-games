eef74394cf6634e8db61dba718bc1d1a1efc7bcf
Shao <oxfoxlion@gmail.com>
2026-09-12 10:56:27 +0800

    day11:基礎設施

--- bomb.js
+++ bomb.js
@@ -1,3 +1,5 @@
+// -----------工具區開始
+
 //地圖初始化工具
 function createMap(width, height) {
 
@@ -24,9 +26,6 @@ function createMap(width, height) {
 
     return map
 }
-
-// 執行地圖初始化並計算出 map 
-const map = createMap(15, 9);
 
 // occupied 預設是一個空陣列，這樣也可以避免萬一沒傳入變數的情況，不會報錯
 function pickRandomEmptyCell(map, occupied = []) {
@@ -81,12 +80,6 @@ function createBoxes(map,boxesNum, occupied = []) {
     return boxes;
 }
 
-let player = pickRandomEmptyCell(map);
-let enemy1 = pickRandomEmptyCell(map,[player]);
-let enemy2 = pickRandomEmptyCell(map,[player,enemy1]);
-let enemy3 = pickRandomEmptyCell(map,[player,enemy1,enemy2]);
-let boxes = createBoxes(map,10,[player,enemy1,enemy2,enemy3]);
-
 //渲染畫面
 function render() {
 
@@ -102,11 +95,11 @@ function render() {
             if (player.x === x && player.y === y) {
                 row += `\x1b[33m@\x1b[0m`
             }else if (enemy1.x === x && enemy1.y === y) {
-                row += `\x1b[31m&\x1b[0m`
+                row += `\x1b[35m&\x1b[0m`
             }else if (enemy2.x === x && enemy2.y === y) {
-                row += `\x1b[31m&\x1b[0m`
+                row += `\x1b[35m&\x1b[0m`
             }else if (enemy3.x === x && enemy3.y === y) {
-                row += `\x1b[31m&\x1b[0m`
+                row += `\x1b[35m&\x1b[0m`
             }else if (boxes.some(box => box.x === x && box.y === y)) {
                 row += `\x1b[34mX\x1b[0m`  // 用藍色和 X 來呈現箱子
             } else {
@@ -118,11 +111,87 @@ function render() {
         frame += row + '\n'
     }
 
-    process.stdout.write(`\x1b[${map.length}A`); // 回到開頭
+    process.stdout.write(`\x1b[${map.length +1 }A`); // 加上儀表板這一行也要一起重新渲染
+    process.stdout.write(`\x1b[31mHP:${HP}\x1b[35m 敵人:${enemyNum}\x1b[0m\n`); // 儀表版
     process.stdout.write(frame); // 印出畫面
 
 }
 
+function movePlayer(dx, dy) {
+    // 藉由傳入的 dx 和 dy 來定位下一個座標，這樣我們就知道要往哪邊走
+    const nextX = player.x + dx; // 下一個 x  
+    const nextY = player.y + dy; // 下一個 y 
+
+    // 找到是誰位於下一個位置，記錄下來(這邊用傳參考的方式，所以修改 firstBox 也會修改到原始值)
+    const hasBox = boxes.find(box => box.x === nextX && box.y === nextY);
+
+    // 前面沒有箱子
+    if (!hasBox) {
+        // 如果前方也是空的
+        if (map[nextY][nextX] === ' ') {
+            player.x = nextX; // player 的座標移動到下一個位置
+            player.y = nextY;
+        }
+        return; // 截斷函式不往下跑
+    }
+}
+
+// -----------工具區結束
+// -----------邏輯區開始
+
+// 執行地圖初始化並計算出 map 
+const map = createMap(15, 9);
+
+// 遊戲物件區
+let player = pickRandomEmptyCell(map);
+let enemy1 = pickRandomEmptyCell(map,[player]);
+let enemy2 = pickRandomEmptyCell(map,[player,enemy1]);
+let enemy3 = pickRandomEmptyCell(map,[player,enemy1,enemy2]);
+let boxes = createBoxes(map,10,[player,enemy1,enemy2,enemy3]);
+let HP = 2;
+let enemyNum = 1;
+
 //初次渲染先推出固定的行數
-process.stdout.write("\n".repeat(map.length));
-render()
+process.stdout.write("\n".repeat(map.length +1));
+render();
+
+// 監聽器前置設定
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.setRawMode(true);
+// 接收到這些狀況的時候被觸發，'data' 指有新資料進來的時候
+process.stdin.on('data', (key) => {
+
+    // 如果按下字母 q 或是 Q，退出遊戲
+    if (key === 'q' || key === 'Q') {
+        console.log("\n遊戲結束，謝謝遊玩！");
+        process.exit();
+    }
+
+    // 如果按下 Ctrl + C 強制退出遊戲
+    if (key === '\x03') {
+        process.exit();
+    }
+
+    // 四個方向
+    if (key === '\x1b[A') {
+        movePlayer(0, -1)
+    }
+    if (key === '\x1b[B') {
+        movePlayer(0, 1)
+    }
+    if (key === '\x1b[C') {
+
+        movePlayer(1, 0)
+    }
+    if (key === '\x1b[D') {
+        movePlayer(-1, 0)
+    }
+
+    // 重新渲染
+    render();
+
+    
+});
+
+
